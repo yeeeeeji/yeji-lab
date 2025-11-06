dccd07f2a6f206932133cf28b1628572a06b59ad
hallo27 <hallo27@naver.com>
2025-11-06 18:02:18 +0900

    chore: react-lab에 TypeScript 적용

---
+++ react-lab/global.d.ts
@@ -0,0 +1,13 @@
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.css";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.js";
