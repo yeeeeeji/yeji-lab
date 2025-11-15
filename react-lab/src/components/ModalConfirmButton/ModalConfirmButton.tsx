0c0422c04e7e7f52d9068f130bf9cd54c666edd6
hallo27 <hallo27@naver.com>
2025-11-16 01:40:33 +0900

    [react] style: ModalConfirmButton 스타일 추가

---
+++ react-lab/src/components/ModalConfirmButton/ModalConfirmButton.tsx
@@ -0,0 +1,5 @@
+import { ModalConfirmButtonStyle } from "./ModalConfirmButtonStyle.styled";
+
+export const ModalConfirmButton = () => {
+  return <ModalConfirmButtonStyle>확인</ModalConfirmButtonStyle>;
+};
---
+++ react-lab/src/components/ModalConfirmButton/ModalConfirmButtonStyle.styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ModalConfirmButtonStyle = styled.button`
+  width: 150px;
+  height: 80px;
+  border: 1px solid black;
+  border-radius: 20px;
+`;
